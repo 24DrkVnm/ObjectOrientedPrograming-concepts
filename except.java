9f395272553e9baf6c7d61bbf988497c7073c760
Alwin Jose <138508113+24DrkVnm@users.noreply.github.com>
2024-09-26 12:05:27 +0530

    Add files via upload

---
+++ except.java
@@ -0,0 +1,52 @@
+package abstra;
+import java.util.*;
+
+public class except {
+	public static void main(String[]args)
+	{
+		Scanner sc = new Scanner(System.in);
+		char ch='a';
+		System.out.println("Choose Your operation 1:Arithmetic 2:Array");
+		int choice=sc.nextInt();
+		do {
+		switch(choice) 
+		{
+		case 1:
+		
+			try
+			{
+				System.out.println("Enter the numerator");
+				int numer=sc.nextInt();
+				System.out.println("Enter the numerator");
+				int denumer=sc.nextInt();
+				int result=numer/denumer;
+				System.out.println("Result:"+result);
+			}catch(ArithmeticException e)
+			{
+				System.out.println("Exception  found: "+e.getMessage());
+			}
+
+				
+		
+		case 2:
+			int []arr= {1,2,3};
+			try 
+			{
+				
+				System.out.println("Enter the array to be displayed");
+				int ind=sc.nextInt();
+				System.out.println("Element is"+arr[ind]);
+			}catch(ArrayIndexOutOfBoundsException e) 
+			{
+				System.out.println("Exception found: "+ e.getMessage());
+			}
+			break;
+		}
+		
+	
+		System.out.println("Do you want to enter another number ?");
+		ch=sc.next().charAt(0);
+		}
+		while(ch=='y'||ch=='Y');
+		sc.close();
+}}
